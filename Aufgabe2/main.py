d053af4b12ef42e703d70b228cb31909e95b6a59
Cem Bicer <e1425692@student.tuwien.ac.at>
2018-08-26 17:30:14 +0200

    Parsing implemented

--- Aufgabe2/main.py
+++ Aufgabe2/main.py
@@ -1,16 +1,49 @@
 from pslang_classes import *
+from helper_classes import *
+import sys
+
 
 def run():
     # TODO: implement
     '''
     PARSING STARTS HERE
 
-    1. read arguments: program (set of rules) and entry point (set of goals)
-    2. parse rules and goals
-    3. for each goal find corresponding rule to execute: parse head, body and return values of rule
-    4. execute parsed rule
+    1. read arguments: program (set of rules) and entry point (initial instructions)
+    2. parse rules and initial instructions and put into queue
+    3. for each instruction find corresponding rule to execute: parse head, body and return values of rule
+    4. execute parsed rule and next instructions into queue
     '''
-    pass
+    if len(sys.argv) != 3:
+        print("Usage:\npython main.py <FILE-CONTAINING-PROGRAM> <FILE-CONTAINING-FIRST-GOAL(S)>")
+        exit(1)
+    with open(sys.argv[1], 'r') as file_program:
+        str_program = file_program.read()
+    with open(sys.argv[2], 'r') as file_initial_instructions:
+        str_initial_instructions = file_initial_instructions.read()
+    parser = Parser()
+    executor = Executor()
+    rules = parser.parse_rules(str_program)
+    initial_instructions = parser.parse_instructions(str_initial_instructions)
+
+    exec_queue = Queue()
+    for i in initial_instructions:
+        # put all initial instructions into the queue
+        exec_queue.enqueue(i)
+    while not exec_queue.is_empty():
+        instr = exec_queue.dequeue()
+        for rule in rules:
+            if not Matcher.match_instruction_rule(instr, rule):
+                # if instruction and rule does not match, ignore
+                continue
+            # found rule to execute
+            if rule.is_shell:
+                # TODO: execute shell command
+                pass
+            else:
+                # put all instructions into the queue
+                for i in rule.body.instructions:
+                    exec_queue.enqueue(i)
+
 
 if __name__ == "__main__":
     run()
--- Aufgabe2/pslang_classes.py
+++ Aufgabe2/pslang_classes.py
@@ -1,7 +1,16 @@
 class Rule:
-    def __init__(self, name, body):
+    def __init__(self, is_shell, name, params, instructions, ret):
+        self.is_shell = is_shell
         self.name = name
-        self.body = body
+        self.body = Body(params, instructions, ret)
+
+
+class Instruction:
+    def __init__(self, is_shell, name, params, ret):
+        self.is_shell = is_shell
+        self.name = name
+        self.body = Body(params, None, ret)
+
 
 class Body:
     def __init__(self, params, instructions, ret):
@@ -9,40 +18,287 @@ class Body:
         self.instructions = instructions
         self.ret = ret
 
-class Instruction:
-    def __init__(self, is_shell, name, params, ret):
-        self.is_shell = is_shell
+
+class Param:
+    def __init__(self, name, ptype):
         self.name = name
-        self.params = params
-        self.ret = ret
+        self.ptype = ptype
+
 
 class Parser:
     def __init__(self):
         pass
 
-    def parseRules(self, str_rules):
-        # TODO: read string and return list of Rules
+    def is_shell(self, str_instruction):
+        '''
+        checks if instruction (as string) is a shell instruction
+
+        :param str_instruction:
+        :return:
+        '''
+        return str_instruction[0] == '$'
+
+    def get_name_of_raw_instruction(self, str_instruction):
+        '''
+        gets instruction (as string) and returns the name (without $ sign)
+
+        :param str_instruction:
+        :return:
+        '''
+        tmp = str_instruction
+        if self.is_shell(str_instruction):
+            tmp = tmp[1:]
+        return self.first_alphabetical_substring(tmp)
+
+    def get_params_of_raw_instruction(self, str_instruction):
+        '''
+        gets instruction (as string) and returns a list of all parameters
+
+        :param str_instruction:
+        :return:
+        '''
+        idx_opening_parantheses = -1
+        for i, c in enumerate(str_instruction):
+            if c.isalpha() or c == '$':
+                continue
+            idx_opening_parantheses = i
+            break
+        idx_closing_parantheses = self.find_first_non_escaped_char(str_instruction, ')')
+        str_params = str_instruction[idx_opening_parantheses + 1:idx_closing_parantheses]
+        return self.parse_params(str_params)
+
+    def get_return_of_raw_instruction(self, str_instruction):
+        '''
+        gets instruction (as string) and returns return value
+
+        :param str_instruction:
+        :return:
+        '''
+        return str_instruction[self.find_last_non_escaped_char(str_instruction, '(') + 1:-1]
+
+    def first_alphabetical_substring(self, string):
+        '''
+        finds the first occurring alphabetical substring of a string
+
+        :param string:
+        :return:
+        '''
+        result = []
+        for c in string:
+            if c.isalpha():
+                result.append(c)
+            else:
+                break
+        return "".join(result)
+
+    def find_all_chars(self, string, char):
+        '''
+        returns a list of all indexes of a character in a string
+
+        :param string:
+        :param char:
+        :return:
+        '''
+        return [i for i, c in enumerate(string) if c == char]
+
+    def find_first_non_escaped_char(self, string, char):
+        '''
+        returns the index of the first non-escaped occurrence of char in a string
+        calls the method find_first_match() to do the actual searching
+
+        :param string:
+        :param char:
+        :return:
+        '''
+        idx = self.find_all_chars(string, char)
+        return self.find_first_match(idx, string)
+
+    def find_last_non_escaped_char(self, string, char):
+        '''
+        returns the index of the last non-escaped occurrence of char in a string
+        calls the method find_first_match() to do the actual searching
+
+        :param string:
+        :param char:
+        :return:
+        '''
+        idx = self.find_all_chars(string, char)
+        reversed = idx[::-1]
+        return self.find_first_match(reversed, string)
+
+    def find_first_match(self, idxs, string):
+        '''
+        returns returns the first non-escaped occurrence of char in a string
+
+        :param idxs:
+        :param string:
+        :return:
+        '''
+        for i in idxs:
+            if i == 0:
+                return 0
+            if string[i - 1] == '\\':
+                continue
+            else:
+                return i
+
+    def escaped_split(self, s, delim):
+        '''
+        source: https://stackoverflow.com/questions/18092354/python-split-string-without-splitting-escaped-character
+
+        splits a string by a delimiter ONLY if it is not escaped
+
+        :param s:
+        :param delim:
+        :return:
+        '''
+        # split by escaped, then by not-escaped
+        escaped_delim = '\\' + delim
+        sections = [p.split(delim) for p in s.split(escaped_delim)]
+        ret = []
+        prev = None
+        for parts in sections:  # for each list of "real" splits
+            if prev is None:
+                if len(parts) > 1:
+                    # Add first item, unless it's also the last in its section
+                    ret.append(parts[0])
+            else:
+                # Add the previous last item joined to the first item
+                ret.append(escaped_delim.join([prev, parts[0]]))
+            for part in parts[1:-1]:
+                # Add all the items in the middle
+                ret.append(part)
+            prev = parts[-1]
+        return ret
+
+    def get_name_of_raw_rule(self, str_rule):
+        '''
+        gets a rule (as string) and returns the name of it
+
+        :param str_rule:
+        :return:
+        '''
+        idx_semicolon = str_rule.find(':')
+        if self.is_shell(str_rule):
+            return str_rule[1:idx_semicolon]
+        else:
+            return str_rule[:idx_semicolon]
+
+    def get_params_of_raw_rule(self, str_rule):
+        '''
+        gets a rule (as string) and returns a list of all parameters (as Parameter instances)
+
+        :param str_rule:
+        :return:
+        '''
+        idx_semicolon = str_rule.find(':')
+        idx_params_end = self.find_first_non_escaped_char(str_rule, ')')
+        str_params = str_rule[idx_semicolon + 2:idx_params_end]
+        return self.parse_params(str_params)
+
+    def get_instructions_of_raw_rule(self, str_rule):
+        '''
+        gets a rule (as string) and returns a list of all instructions (as Instruction instances)
+
+        :param str_rule:
+        :return:
+        '''
+        if self.is_shell(str_rule):
+            prefix = '$'
+        else:
+            prefix = ""
+        name = self.get_name_of_raw_rule(str_rule)
+        params = self.get_params_of_raw_rule(str_rule)
+        length = len(prefix + name + ':' + '(' + ','.join(p.name for p in params) + ')')
+        str_instr = str_rule[length + 1:self.find_last_non_escaped_char(str_rule, '(') - 1]
+        if self.is_shell(str_rule):
+            return [str_instr]
+        else:
+            return self.parse_instructions(str_instr)
+
+    def parse_params(self, str_params):
+        '''
+        gets parameters (as string) and returns a list of Parameter instances
+
+        :param str_params:
+        :return:
+        '''
+        params_split = str_params.split(',')
+        params = []
+        for p in params_split:
+            par = Param(p, "")
+            if '*' in p or len(p.strip().split(' ')) > 1:
+                par.ptype = "OPEN"
+            else:
+                par.ptype = "CLOSED"
+            params.append(par)
+        return params
+
+    def parse_rules(self, str_rules):
+        '''
+        gets all rules (as string) and returns a list of Rule instances
+
+        :param str_rules:
+        :return:
+        '''
+        str_rules = self.escaped_split(str_rules, '.')
+        str_rules = (rule.strip('\n') for rule in str_rules)
+        rules = []
+        for str_rule in str_rules:
+            parsed = Rule(self.is_shell(str_rule), self.get_name_of_raw_rule(str_rule),
+                          self.get_params_of_raw_rule(str_rule), self.get_instructions_of_raw_rule(str_rule),
+                          self.get_return_of_raw_instruction(str_rule))
+            rules.append(parsed)
+        return rules
+
+    def parse_instructions(self, str_instructions):
+        '''
+        gets instructions (as string) and returns a list of Instruction instances
+
+        :param str_instructions: instructions of the form "INSTRUCTION1,INSTRUCTION2,..."
+        :return: list of parsed instruction instances
+        '''
+        instructions_split = self.escaped_split(str_instructions, ';')
+        instructions = []
+        for str_instr in instructions_split:
+            parsed = Instruction(self.is_shell(str_instr), self.get_name_of_raw_instruction(str_instr),
+                                 self.get_params_of_raw_instruction(str_instr),
+                                 self.get_return_of_raw_instruction(str_instr))
+            instructions.append(parsed)
+        return instructions
+
+
+class Matcher:
+    def __init__(self):
         pass
 
-    def getBody(self, rule):
-        # TODO: read Rule and return Body of Rule
-        pass
+    @staticmethod
+    def match_instruction_rule(instr, rule):
+        '''
+        checks if an instruction instance matches an rule instances by checking
+        the name and number of parameters and if both are shell commands
 
-    def getParams(self, body):
-        # TODO: read Body and return list of params (strings)
-        pass
+        :param instr: instruction instance
+        :param rule: rule instance
+        :return: true if both instances have the same name and number of parameters and if both are shell commands
+        '''
+        return instr.is_shell == rule.is_shell and instr.name == rule.name and len(instr.body.params) == len(
+            rule.body.params) and Matcher.check_param_types(instr.body.params, rule.body.params)
 
-    def getInstructionUnparsed(self, body):
-        # TODO: read Body and return list of instructions (raw strings, UNPARSED)
-        pass
+    @staticmethod
+    def check_param_types(instr_params, rule_params):
+        '''
+        checks if the types of all parameters of a Instruction instance and a Rule instance match
 
-    def getReturn(self, body):
-        # TODO: read Body and return Body's return value as string
-        pass
+        :param instr_params:
+        :param rule_params:
+        :return:
+        '''
+        for ip, rp in zip(instr_params, rule_params):
+            if ip.ptype != rp.ptype:
+                return False
+        return True
 
-    def parseInstructions(self, str_instructions):
-        # TODO: read string and return list of Instructions
-        pass
 
 class Executor:
     def __init__(self):
