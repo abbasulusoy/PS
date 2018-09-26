99fdc26e2ca22246ae49b7e866b4b1a6b65f3e6a
Abbas ULUSOY <abbasulusoy@hotmail.com>
2018-09-26 15:47:46 +0200

    [ABUL] fixed test

--- Aufgabe1/src/main/java/ps/abgabe/StackCalculator.java
+++ Aufgabe1/src/main/java/ps/abgabe/StackCalculator.java
@@ -1,10 +1,9 @@
 package ps.abgabe;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
-import javax.naming.spi.DirStateFactory.Result;
 
 /**
  * @author Abbas ULUSOY fill stacks with operators and values
@@ -12,94 +11,94 @@ import javax.naming.spi.DirStateFactory.Result;
  */
 public class StackCalculator {
 
-	DataStack<String> outputs = new DataStack<String>();
-	DataStack<String> inputs = new DataStack<String>();
-	Calculator calculator = new Calculator();
+    DataStack<String> outputs = new DataStack<String>();
+    DataStack<String> inputs = new DataStack<String>();
+    Calculator calculator = new Calculator();
 
-	/**
-	 * Evaluates the expression
-	 * 
-	 * @param inputStream
-	 *            input linked list, each operator/number is stored in the list
-	 *            as an element
-	 * @return string - result as a string
-	 */
-	public void evaluate(String expression) {
-		String[] exprArray = null;
-		DataStack<String> inputStream = new DataStack<>();
-		DataStack<String> outputs = new DataStack<>();
+    /**
+     * Evaluates the expression
+     * 
+     * @param inputStream
+     *            input linked list, each operator/number is stored in the list
+     *            as an element
+     * @return string - result as a string
+     */
+    public void evaluate(String expression) {
+        String[] exprArray = null;
+        DataStack<String> inputStream = new DataStack<>();
+        DataStack<String> outputs = new DataStack<>();
+        outputs.push("0");
 
-		inputStream.push(expression.substring(expression.length() - 1));
-		expression = expression.substring(0, expression.length() - 1);
-		outputs.push("3");
+        exprArray = expression.split("");
+        String resul = "";
 
-		exprArray = expression.split("");
-		String resul="";
-		for (int i = 0; i < expression.length(); i++) {
-			if (expression.charAt(i) == '(') {
-				String list = generateList(expression);
-				if(!list.isEmpty()) {
-					outputs.push(list);
-					expression = expression.replace(list, "");
-					i=0;
-				} 
-				
-			} else {
-				outputs.push(expression.charAt(i)+"");
-			}
-		}
-		
-		
-		for (String s : outputs.getList()) {
-			System.out.println("Item ::" + s);
-		}
+        while (expression.contains("(")) {
+            String list = generateList(expression, outputs);
+            if (!list.isEmpty()) {
+                outputs.push(list);
+                expression = expression.replace(list, "");
 
-		Iterator<String> iter = inputStream.iterator();
+            }
+        }
 
-		while (iter.hasNext()) {
-			String token = inputStream.pop();
-			if (calculator.isUnaryOperator(token)) {
-				calculator.evaluateUnaryOperator(token, inputStream, outputs);
-			} else if (calculator.isBinaryOperator(token)) {
-				calculator.evaluateOperator(token, outputs);
-			} else if (calculator.isNumeric(token)) {
-				outputs.push(token);
-			}
-		}
-		
-		for (String s : outputs.getList()) {
-			System.out.println("Result ::" + s);
-		}
-	}
+        if (!expression.contains("(")) {
+            for (String s : expression.split("")) {
+                if (!s.isEmpty()) {
+                    inputStream.push(s);
+                }
+            }
+            Collections.reverse(inputStream.getList());
+        }
+        for (String s : inputStream.getList()) {
+            System.out.println("Item ::" + s);
+        }
 
-	public List<String> getStringFromListToExecute(String strFromList) {
-		List<String> separteList = Arrays.asList(strFromList.split(""));
-		return separteList;
-	}
+        Iterator<String> iter = inputStream.iterator();
 
-	public static String generateList(String expression) {
+        while (iter.hasNext()) {
+            String token = inputStream.pop();
+            if (calculator.isUnaryOperator(token)) {
+                calculator.evaluateUnaryOperator(token, inputStream, outputs);
+            } else if (calculator.isBinaryOperator(token)) {
+                calculator.evaluateOperator(token, outputs);
+            } else if (calculator.isNumeric(token)) {
+                outputs.push(token);
+            }
+        }
 
-		int open = 0;
-		String list = "";
+        for (String s : outputs.getList()) {
+            System.out.println("Result ::" + s);
+        }
+    }
 
-		for (int i = 0; i < expression.length(); i++) {
-			if (expression.charAt(i) == '(') {
-				open++;
-			} else if (expression.charAt(i) == ')') {
+    public List<String> getStringFromListToExecute(String strFromList) {
+        List<String> separteList = Arrays.asList(strFromList.split(""));
+        return separteList;
+    }
 
-				open--;
-			}
-			if (open > 0) {
-				list += expression.charAt(i);
-			} else {
-				list += expression.charAt(i);
-				break;
-			}
-		}
-		return list;
-	}
+    public static String generateList(String expression, DataStack<String> outputs) {
 
-	public static void main(String[] args) {
-		System.out.println(generateList("(2c1 3c-1c1=3c()(3c4d1+da)a2d*)2c3d2ca2d"));
-	}
+        int open = 0;
+        String list = "";
+
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') {
+                open++;
+            } else if (expression.charAt(i) == ')') {
+
+                open--;
+            }
+            if (open > 0) {
+                list += expression.charAt(i);
+            } else {
+                list += expression.charAt(i);
+                break;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateList("(2c1 3c-1c1=3c()(3c4d1+da)a2d*)2c3d2ca2d", new DataStack<String>()));
+    }
 }
